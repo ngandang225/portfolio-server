ca95b82220f76fd5bc46e8313454b0458e2ba363
ngandang225 <ngandang@meowcart.net>
2025-04-19 17:47:21 +0700

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
